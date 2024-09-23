1953e6c84ec4f40ec68967190e8700e9fc178480
patrickwitmer <patrick@witmerdesigns.com>
2024-09-23 15:11:18 -0600

    new chapter 11 items

---
+++ Chapter 11 - Debugging/practiceQuestions.py
@@ -0,0 +1,11 @@
+# Write an assert statement that triggers an AssertionError if the variable spam is an integer less than 10.
+
+
+spam = 5
+
+assert spam > 10
+
+ages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ages.sort()
+
+assert ages[0] <= ages[-1]
